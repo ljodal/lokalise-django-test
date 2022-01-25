fdec48269c912367c4b893bab1ce6b3b130f1840
Sigurd Ljødal <544451+ljodal@users.noreply.github.com>
2022-01-25 23:41:33 +0100

    Initial commit

---
+++ project/app/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpRequest, HttpResponse
+from django.utils.translation import get_language
+from django.utils.translation import gettext as _
+
+
+def my_view(request: HttpRequest) -> HttpResponse:
+    print(get_language())
+    print(_("Foo"))
+
+    text = _("Hello {name}, I'm Django!").format(
+        name=request.GET.get("name", _("world"))
+    )
+    return HttpResponse(text)
