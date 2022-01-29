70982089afc3e0b80ff95c97c92ddbdfcb5b6e8b
Sigurd Ljødal <544451+ljodal@users.noreply.github.com>
2022-01-29 23:49:24 +0100

    Cleanup and add view template view

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import my_view
+from .views import my_view, template_view
 
 urlpatterns = [
     path("", my_view, name="index"),
+    path("test", template_view, name="test"),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpRequest, HttpResponse
-from django.utils.translation import get_language
+from django.shortcuts import render
 from django.utils.translation import gettext as _
 
 
 def my_view(request: HttpRequest) -> HttpResponse:
-    print(get_language())
-    print(_("Foo"))
 
     # Translators: This is a test
     text = _("Hello {name}, I'm Django!").format(
         name=request.GET.get("name", _("world"))
     )
     return HttpResponse(text)
+
+
+def template_view(request: HttpRequest) -> HttpResponse:
+
+    return render(request, "test.html", {"name": request.GET.get("name", _("world"))})
